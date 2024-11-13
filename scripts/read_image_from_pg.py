bca12f5ac0a67d3b806cad81fbb47e755d121029
Patricio Bonilla <gpbonillas@gmail.com>
2024-11-13 14:30:32 -0500

    Edit script to plot all image in one figure

--- scripts/read_image_from_pg.py
+++ scripts/read_image_from_pg.py
@@ -1,9 +1,7 @@
-from os.path import exists
 from PIL import Image
 import psycopg2
 import io
 import matplotlib.pyplot as plt
-import numpy as np
 
 # connect to the database
 conn = psycopg2.connect(
@@ -17,33 +15,26 @@ conn = psycopg2.connect(
 # Create a cursor
 cur = conn.cursor()
 
-
 # check if table exists
 cur.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'image_files')")
 if cur.fetchone()[0]:
     print("Table '%s' exists" % 'image_files')
 
 # read the images from the database
-cur.execute("SELECT image_id, path_image, image_file FROM image_files WHERE image_id = '0009051b-a76c-4a27-ac5d-bf410aea7e0c'")
+cur.execute("SELECT image_id, path_image, image_file, ROW_NUMBER () OVER (ORDER BY image_id) as i FROM image_files LIMIT 10")
 if cur.rowcount == 0:
     print("No images in the database")
     exit()
 
-for image_id, path_image, image_file in cur.fetchall():
+plt.figure(figsize=(20, 10))
 
-    print("image_id: ", image_id, "path_image: ", path_image, "exist_image: ", exists(path_image))
+for image_id, path_image, image_file, i in cur.fetchall():
+    # plot all images in one plot figure with 2 rows and 5 columns
+    plt.subplot(2, 5, i)
+    # read the image file from the database
+    image = Image.open(io.BytesIO(image_file))
+    plt.imshow(image)
+    plt.title(f"image: {i}")
+    plt.axis('off')
 
-    if image_id == '0009051b-a76c-4a27-ac5d-bf410aea7e0c':
-        # read the image file in binary mode
-        image = Image.open(io.BytesIO(image_file))
-        print(image.size)
-        print(image.mode)
-        print(image.format)
-
-        # plot the image
-        # plt.close(1)
-        fig = plt.figure(1)
-        plt.imshow(image)
-        plt.title("Image ID: %s " % (image_id))
-        plt.axis('off')
-        plt.show()
+plt.show()
